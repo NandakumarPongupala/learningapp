1a25ba9a8eab01c5427b16228c8fb904ae1a692b
Nandakumar Pongupala <nandakumar.pongupala@neudesic.com>
2026-01-06 19:09:35 +0530

    Final Test on 6-Jan-2026

--- learningapp/obj/Release/net8.0/learningapp.AssemblyInfo.cs
+++ learningapp/obj/Release/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6f52c5c3912ff6de3dc3aa29f9b23b25cf2e87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290f8170542da6e17451e9ccaac07f488054cf2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
